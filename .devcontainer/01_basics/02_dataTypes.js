87d51b7395b5b461d95d3a2dbdaaca259701b81e
gurureddy06 <guruviranjan@gmail.com>
2025-05-22 10:02:59 +0000

    data types

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -4,18 +4,57 @@ let isLoggedIn = false
 
 /*
 
-data types
-number => 2 to power 53
-bigint
-string => ""
-boolean => true/false
+primitive data types
+
+Number => 2 to power 53
+BigInt
+String => ""
+Boolean => true/false
 null => standalone value
 undefined
-symbol => unique
+Symbol => unique
 
-object
+Non-primitive data types
+
+Array
+Objects
+Functions
 
 */
 console.log(typeof undefined); //undefined
 console.log(typeof null); //object
 
+// ******* Memories ******* //
+
+// stack (Primitive), Heap (Non-Primitive)
+
+//primitive datatypes are stored in stack memory 
+
+let myName = "guru"
+
+let anotherName = myName
+anotherName = "partiv"
+
+console.log(anotherName);
+console.log(myName);
+
+let userOne = {
+    email_id : "guru@gmil.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "partiv@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+/*
+
+Feature	                Stack	                       Heap
+Stores	       Primitives & references	        Objects, arrays, functions
+Size	         Small and limited	              Large and dynamic
+Speed	                Fast	                  Slower than stack
+Allocation	  Automatically via call stack	    Dynamically when needed
+
+*/
