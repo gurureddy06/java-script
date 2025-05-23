3a41425c47d1707af5435e826b295ca4316aab81
gurureddy06 <guruviranjan@gmail.com>
2025-05-23 09:25:46 +0000

    Arrays

--- .devcontainer/02_basics/01_Arrays.js
+++ .devcontainer/02_basics/01_Arrays.js
@@ -1,24 +1,24 @@
 const myArr = [0, 1, 2, 3, 4, 5]
-//console.log(myArr[0]);
+console.log(myArr[0]);
 
-// const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
 
-// Array methods
+//Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9)
+myArr.shift()
 
-// console.log(myArr.includes(3));
+console.log(myArr.includes(3));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log(newArr);
+console.log(myArr);
+console.log(newArr);
 
 // slice, splice
 
